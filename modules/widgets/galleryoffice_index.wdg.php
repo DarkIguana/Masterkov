72a33e1c71fd7388ac1be969a9430af4eb5fdc0b
saam <samvietreg@gmail.com>
2019-03-25 22:51:17 +0700

    slider office

---
+++ modules/widgets/galleryoffice_index.wdg.php
@@ -0,0 +1,25 @@
+<?php 
+class galleryoffice_index {
+	
+	function __construct(){
+		$out.='  
+                    <div class="box" id="vmc-slide">
+    <ul>
+        <li title="Ремонт офисов"><img src="/img/slider_office/masterkov-remont-ofisov-01.jpg"></li>
+        <li title="Ремонт офисов"><img src="/img/slider_office/masterkov-remont-ofisov-02.jpg"></li>
+        <li title="Ремонт офисов"><img src="/img/slider_office/masterkov-remont-ofisov-03.jpg"></li>
+        <li title="Ремонт офисов"><img src="/img/slider_office/masterkov-remont-ofisov-04.jpg"></li>
+    </ul>
+</div>  ';
+                    
+			$this->over = $out;
+		}
+	}
+
+?>
+
+
+		
+			
+		
+	
